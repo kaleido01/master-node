195d1206fe167024bb51d9d5b8ab78850250f886
kaleido <kaleido@gmail.com>
2019-05-02 14:21:58 +0900

    routing について

--- section5/app.js
+++ section5/app.js
@@ -2,13 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-	console.log("in the middleware");
-	next(); //次のミドルウェアへ進むことを可能にする
+app.use("/", (req, res, next) => {
+	console.log("always");
+	//自動でheaderが付加される
+	next();
 });
-app.use((req, res, next) => {
-	console.log("in the another middleware");
 
+app.use("/add-product", (req, res, next) => {
+	console.log("in the another");
+	//自動でheaderが付加される
+	res.send("<h1>the add product</h1>");
+});
+app.use("/", (req, res, next) => {
+	console.log("in the another middleware");
 	//自動でheaderが付加される
 	res.send("<h1>hhhhhhhhaaaaaaaaiiiii</h1>");
 });
