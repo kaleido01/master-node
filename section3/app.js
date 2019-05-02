9290aa4bdf50884f44c3a4962f4e89e9e0d9644d
kaleido <kaleido@gmail.com>
2019-05-02 11:52:04 +0900

    request bodyの内容について

--- section3/app.js
+++ section3/app.js
@@ -17,7 +17,20 @@ const server = http.createServer((req, res) => {
 	}
 	// process.exit()
 	if (url === "/message" && method === "POST") {
-		fs.writeFileSync("message.txt", "DUMMY");
+		const body = [];
+
+		//入ってくるデータを読み込む一行ずつ
+		req.on("data", chunk => {
+			console.log(chunk);
+			body.push(chunk);
+		});
+
+		req.on("end", () => {
+			const parsedBody = Buffer.concat(body).toString();
+			const message = parsedBody.split("=")[1];
+			fs.writeFileSync("message.txt", message);
+		});
+
 		res.statusCode = 302;
 		res.setHeader("Location", "/");
 		return res.end();
