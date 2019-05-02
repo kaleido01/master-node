20d05d8b1e9513299c4f955d87c6af199a7f2e56
kaleido <kaleido@gmail.com>
2019-05-02 10:21:00 +0900

    asyncの勉強

---
+++ section2/async.js
@@ -0,0 +1,25 @@
+//2秒後にコードが実行される
+setTimeout(() => {
+	console.log("Timer is done!");
+}, 2000);
+
+const fetchData = () => {
+	const promise = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve("Done!");
+		}, 1500);
+	});
+	return promise;
+};
+
+setTimeout(() => {
+	console.log("Timer is done");
+	fetchData()
+		.then(text => {
+			console.log(text);
+			return fetchData();
+		})
+		.then(text2 => {
+			console.log(text2);
+		});
+}, 2000);
