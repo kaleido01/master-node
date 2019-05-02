041cba8c2a20891a5e3e02f8fc1bd277c002fe34
kaleido <kaleido@gmail.com>
2019-05-02 11:29:04 +0900

    resのHeaderにコンテンツを設定する方法

--- section3/app.js
+++ section3/app.js
@@ -3,6 +3,13 @@ const http = require("http");
 const server = http.createServer((req, res) => {
 	console.log(req.url, req.method, req.headers);
 	// process.exit()
+	res.setHeader("Content-Type", "text/html");
+	res.write("<html>");
+	res.write("<head><Title>My first Page</Title></head>");
+	res.write("<body><h1>Hello my node.js Server!</h1></body>");
+	res.write("</html>");
+
+	res.end();
 });
 
 server.listen(3000);
