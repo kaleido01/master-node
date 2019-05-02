0e189735c5254c1b8f5bb0475c8cb88ea34d9fdd
kaleido <kaleido@gmail.com>
2019-05-02 14:48:25 +0900

    add bodyparser

--- section5/app.js
+++ section5/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", (req, res, next) => {
 	console.log("always");
@@ -11,8 +14,15 @@ app.use("/", (req, res, next) => {
 app.use("/add-product", (req, res, next) => {
 	console.log("in the another");
 	//自動でheaderが付加される
-	res.send("<h1>the add product</h1>");
+	res.send(
+		"<form action='/product' method='POST'><input type='text'name='title' ><button type='submit'>add product</button></input></form>"
+	);
 });
+app.use("/product", (req, res, next) => {
+	console.log(req.body);
+	res.redirect("/");
+});
+
 app.use("/", (req, res, next) => {
 	console.log("in the another middleware");
 	//自動でheaderが付加される
