4e272ba7d8f2e02fc937ec10f2191ebb81c41883
kaleido <kaleido@gmail.com>
2019-05-02 15:08:00 +0900

    add 404 status and filtering routes

--- section5/routes/admin.js
+++ section5/routes/admin.js
@@ -2,14 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 
+// /admin/add-product =>get
 router.get("/add-product", (req, res, next) => {
 	console.log("in the another");
 	//自動でheaderが付加される
 	res.send(
-		"<form action='/product' method='POST'><input type='text'name='title' ><button type='submit'>add product</button></input></form>"
+		"<form action='/admin/product' method='POST'><input type='text'name='title' ><button type='submit'>add product</button></input></form>"
 	);
 });
-router.post("/product", (req, res, next) => {
+// /admin/add-product =>post
+
+router.post("/add-product", (req, res, next) => {
 	console.log(req.body);
 	res.redirect("/");
 });
