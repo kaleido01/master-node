5dc0a9067ff609bcaa2ce92b2afdc5c73e68f3fa
kaleido <kaleido@gmail.com>
2019-05-02 11:36:07 +0900

    POST methodの作成

--- section3/app.js
+++ section3/app.js
@@ -2,6 +2,17 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
 	console.log(req.url, req.method, req.headers);
+	const url = req.url;
+	if (url === "/") {
+		res.write("<html>");
+		res.write("<head><Title>Enter Message</Title></Title></head>");
+		res.write(
+			"<body><form action='/message' method='POST'><input type='text'name='message' ><button type='submit'>send</button></input></form></body>"
+		);
+		res.write("</html>");
+
+		return res.end();
+	}
 	// process.exit()
 	res.setHeader("Content-Type", "text/html");
 	res.write("<html>");
