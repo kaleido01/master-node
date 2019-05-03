be07a622205f07a92f61354dd2ee9f5f4ad467a7
kaleido <kaleido@gmail.com>
2019-05-04 02:00:09 +0900

    fix bug delete cart item

--- section9/routes/shop.js
+++ section9/routes/shop.js
@@ -11,6 +11,7 @@ router.get("/products", shopConstroller.getProducts);
 router.get("/products/:productId", shopConstroller.getProduct);
 router.get("/cart", shopConstroller.getCart);
 router.post("/cart", shopConstroller.postCart);
+router.post("/cart-delete-item", shopConstroller.postCartDeleteProduct);
 router.get("/orders", shopConstroller.getOrders);
 router.get("/checkout", shopConstroller.getCheckout);
 
