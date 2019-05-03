05fe81e4b40e1b785b3f74d47e43d2ed633a3ad6
kaleido <kaleido@gmail.com>
2019-05-03 10:40:44 +0900

    controller,ロジック部分の切り出し

--- section7/controllers/products.js
+++ section7/controllers/products.js
@@ -1,7 +1,25 @@
+const products = [];
+
 exports.getAddProduct = (req, res, next) => {
 	res.render("add-product", {
 		prods: products,
 		pageTitle: "add products",
+		path: "/",
+		hasProducts: products.length > 0,
+		activeShop: true,
+		productCSS: true
+	});
+};
+
+exports.postAddProduct = (req, res, next) => {
+	products.push({ title: req.body.title });
+	res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+	res.render("shop", {
+		pageTitle: "shop",
+		prods: products,
 		path: "/",
 		hasProducts: products.length > 0,
 		activeShop: true,
--- section7/routes/shop.js
+++ section7/routes/shop.js
@@ -2,22 +2,10 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+const productsConstroller = require("../controllers/products");
 
 const router = express.Router();
 
-const adminData = require("./admin");
-
-router.get("/", (req, res, next) => {
-	products = adminData.products;
-	res.render("shop", {
-		pageTitle: "shop",
-		prods: products,
-		path: "/",
-		hasProducts: products.length > 0,
-		activeShop: true,
-		productCSS: true
-	});
-});
+router.get("/", productsConstroller.getProducts);
 
 module.exports = router;
