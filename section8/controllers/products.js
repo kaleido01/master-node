11a67078ef1a56543911683b002ff3059e94234a
kaleido <kaleido@gmail.com>
2019-05-03 13:36:34 +0900

    navigation の改善

--- section8/controllers/products.js
+++ section8/controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-	res.render(".admin/add-product", {
+	res.render("admin/add-product", {
 		pageTitle: "add products",
 		path: "/",
 		activeShop: true,
