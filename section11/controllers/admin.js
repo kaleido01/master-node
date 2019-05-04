5ed755e3649def9f34cb3fe7cfbf56501a07ee77
kaleido <kaleido@gmail.com>
2019-05-04 15:36:23 +0900

    関連する商品のみを取り出す方法

--- section11/controllers/admin.js
+++ section11/controllers/admin.js
@@ -35,11 +35,15 @@ exports.getEditProduct = (req, res, next) => {
 		return res.redirect("/");
 	}
 	const prodId = req.params.productId;
-	Product.findByPk(prodId)
-		.then(product => {
+	req.user
+		.getProducts({ where: { id: prodId } })
+		// Product.findByPk(prodId)
+		.then(products => {
+			const product = products[0];
 			if (!product) {
 				return res.redirect("/");
 			}
+
 			res.render("admin/edit-product", {
 				pageTitle: "Edit Product",
 				path: "/admin/edit-product",
@@ -72,7 +76,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-	Product.findAll()
+	// Product.findAll()
+	req.user
+		.getProducts()
 		.then(products => {
 			res.render("admin/products", {
 				prods: products,
