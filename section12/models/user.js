ea5d4aa13fa95c4ff3717bf04c8ada05ca2044cb
kaleido <kaleido@gmail.com>
2019-05-06 10:33:43 +0900

    method addToCart

--- section12/models/user.js
+++ section12/models/user.js
@@ -2,9 +2,11 @@ const getDb = require("../util/database").getDb;
 const mongodb = require("mongodb");
 
 class User {
-	constructor(name, email) {
+	constructor(name, email, cart, id) {
 		this.name = name;
 		this.email = email;
+		this.cart = cart; // {items:[]}
+		this._id = id;
 	}
 
 	save() {
@@ -19,6 +21,14 @@ class User {
 			.catch(err => {
 				console.log(err);
 			});
+	}
+
+	addToCart(product) {
+		const updatedCart = { items: [{ ...product, quantity: 1 }] };
+		const db = getDb();
+		return db
+			.collection("users")
+			.updateOne({ _id: new ObjectId(this._id) }, $set({ cart: updatedCart }));
 	}
 
 	static fetchAll() {
