80f3cbe3cdd744dcd6d8a82f726d043f10542152
kaleido <kaleido@gmail.com>
2019-05-06 13:42:17 +0900

    addOrder

--- section12/models/user.js
+++ section12/models/user.js
@@ -77,6 +77,22 @@ class User {
 			});
 	}
 
+	addOrder() {
+		const db = getDb();
+		return db
+			.collection("orders")
+			.insertOne(this.cart)
+			.then(result => {
+				this.cart = { items: [] };
+				return db
+					.collection("users")
+					.updateOne(
+						{ _id: new ObjectId(this._id) },
+						{ $set: { cart: { items: [] } } }
+					);
+			});
+	}
+
 	deleteItemFromCart(productId) {
 		const updatedCartItems = this.cart.items.filter(item => {
 			return item.productId.toString() !== productId.toString();
