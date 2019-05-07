a87223432d60c3b1c9b0cf7934c55ee0d899980d
kaleido <kaleido@gmail.com>
2019-05-07 21:05:38 +0900

    add getPosts

--- session24-restAPI-server/app.js
+++ session24-restAPI-server/app.js
@@ -5,12 +5,17 @@ const feedRoutes = require("./routes/feed");
 
 const app = express();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
 
-app.unsubscribe((req, res, next) => {
+app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
-	res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"OPTIONS, GET, POST, PUT, PATCH, DELETE"
+	);
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	next();
 });
 
 app.use("/feed", feedRoutes);
--- session24-restAPI-server/controllers/feed.js
+++ session24-restAPI-server/controllers/feed.js
@@ -1,13 +1,32 @@
 exports.getPosts = (req, res, next) => {
 	res.status(200).json({
-		posts: [{ title: "First Post", content: "this is the first post" }]
+		posts: [
+			{
+				_id: "1",
+				title: "First Post",
+				content: "This is the first post!",
+				imageUrl: "images/duck.jpg",
+				creator: {
+					name: "Maximilian"
+				},
+				createdAt: new Date()
+			}
+		]
 	});
 };
 
 exports.createPost = (req, res, next) => {
-	const { title, content } = req.body;
+	const title = req.body.title;
+	const content = req.body.content;
+	// Create post in db
 	res.status(201).json({
-		message: "Post created successfully",
-		post: { id: new Date().toISOString(), title, content }
+		message: "Post created successfully!",
+		post: {
+			_id: new Date().toISOString(),
+			title: title,
+			content: content,
+			creator: { name: "Maximilian" },
+			createdAt: new Date()
+		}
 	});
 };
