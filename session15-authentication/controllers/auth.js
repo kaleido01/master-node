6bcf49b53db781430a41caaff9533536397d1394
kaleido <kaleido@gmail.com>
2019-05-08 13:47:07 +0900

    認証の追加bcryptjs追加によるパスワードのハッシュ化

--- session15-authentication/controllers/auth.js
+++ session15-authentication/controllers/auth.js
@@ -1,39 +1,65 @@
-const User = require('../models/user');
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: false
-  });
+	res.render("auth/login", {
+		path: "/login",
+		pageTitle: "Login",
+		isAuthenticated: false
+	});
 };
 
 exports.getSignup = (req, res, next) => {
-  res.render('auth/signup', {
-    path: '/signup',
-    pageTitle: 'Signup',
-    isAuthenticated: false
-  });
+	res.render("auth/signup", {
+		path: "/signup",
+		pageTitle: "Signup",
+		isAuthenticated: false
+	});
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
-    })
-    .catch(err => console.log(err));
+	User.findById("5bab316ce0a7c75f783cb8a8")
+		.then(user => {
+			req.session.isLoggedIn = true;
+			req.session.user = user;
+			req.session.save(err => {
+				console.log(err);
+				res.redirect("/");
+			});
+		})
+		.catch(err => console.log(err));
 };
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+	const { email, password, confirmPassword } = req.body;
+	User.findOne({ email })
+		.then(userDoc => {
+			if (userDoc) {
+				return res.redirect("/signup");
+			}
+
+			//非同期処理
+			return bcrypt.hash(password, 12);
+		})
+		.then(hashedPassword => {
+			const user = new User({
+				email,
+				password: hashedPassword,
+				cart: { items: [] }
+			});
+			return user.save();
+		})
+		.then(result => {
+			res.redirect("/login");
+		})
+		.catch(err => {
+			console.log(err);
+		});
+};
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy(err => {
-    console.log(err);
-    res.redirect('/');
-  });
+	req.session.destroy(err => {
+		console.log(err);
+		res.redirect("/");
+	});
 };
