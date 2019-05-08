cd192427d524a17a8944788fb3223a3d415540d2
kaleido <kaleido@gmail.com>
2019-05-08 14:30:39 +0900

    csrfTokenの生成法とviewへの埋め込み（面倒な方法）

--- session15-authentication/app.js
+++ session15-authentication/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -18,12 +19,16 @@ const store = new MongoDBStore({
 	collection: "sessions"
 });
 
+const csrfProtection = csrf();
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
+
+app.use(csrfProtection());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -55,7 +60,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-	.connect(MONGODB_URI)
+	.connect(MONGODB_URI, { useNewUrlParser: true })
 	.then(result => {
 		app.listen(3000);
 		console.log("connected!");
