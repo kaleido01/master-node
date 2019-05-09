cf74bbf5620e547ddc431f0daf00c52ccc475440
kaleido <kaleido@gmail.com>
2019-05-10 00:56:53 +0900

    deletePost

--- session24-restAPI-server/controllers/feed.js
+++ session24-restAPI-server/controllers/feed.js
@@ -128,6 +128,31 @@ exports.updatePost = (req, res, next) => {
 		});
 };
 
+exports.deletePost = (req, res, next) => {
+	const postId = req.params.postId;
+	Post.findById(postId)
+		.then(post => {
+			if (!post) {
+				const error = new Error("Could not find post");
+				error.statusCode = 404;
+				throw error;
+			}
+			//CHeck logged in user
+			clearImage(post.imageUrl);
+			return Post.findByIdAndRemove(postId);
+		})
+		.then(result => {
+			console.log(result);
+			res.status(200).json({ message: "Delete post succeed" });
+		})
+		.catch(err => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
+};
+
 const clearImage = filePath => {
 	filePath = path.join(__dirname, "..", filePath);
 	fs.unlink(filePath, err => console.log(err));
