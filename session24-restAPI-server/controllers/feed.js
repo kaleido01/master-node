7537482fdf121e0e54442384a93e8213a55369bd
kaleido <kaleido@gmail.com>
2019-05-10 00:44:44 +0900

    前imageの削除+edit機能の追加

--- session24-restAPI-server/controllers/feed.js
+++ session24-restAPI-server/controllers/feed.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const { validationResult } = require("express-validator/check");
 const Post = require("../model/post");
 
@@ -60,7 +62,6 @@ exports.createPost = (req, res, next) => {
 
 exports.getPost = (req, res, next) => {
 	const postId = req.params.postId;
-	console.log(postId);
 	Post.findById(postId)
 		.then(post => {
 			if (!post) {
@@ -79,5 +80,55 @@ exports.getPost = (req, res, next) => {
 };
 
 exports.updatePost = (req, res, next) => {
-	const imageUrl = req.file.path.replace("\\", "/");
+	const postId = req.params.postId;
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		const error = new Error("Validation failed, entered datais incorrect");
+		error.statusCode = 422;
+		throw error;
+	}
+	const title = req.body.title;
+	const content = req.body.content;
+	let imageUrl = req.body.image;
+	if (req.file) {
+		imageUrl = req.file.path.replace("\\", "/");
+	}
+	if (!imageUrl) {
+		const error = new Error("No file picked");
+		error.statusCode = 422;
+		throw error;
+	}
+
+	Post.findById(postId)
+
+		.then(post => {
+			if (!post) {
+				const error = new Error("Could not find post");
+				error.statusCode = 404;
+				throw error;
+			}
+			if (!imageUrl !== post.imageUrl) {
+				clearImage(post.imageUrl);
+			}
+			post.title = title;
+			post.imageUrl = imageUrl;
+			post.content = content;
+			return post.save();
+		})
+		.then(post => {
+			res.status(200).json({ message: "Post updated", post });
+		})
+
+		.catch(err => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
+};
+
+const clearImage = filePath => {
+	filePath = path.join(__dirname, "..", filePath);
+	fs.unlink(filePath, err => console.log(err));
 };
