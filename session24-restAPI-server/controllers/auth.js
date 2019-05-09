ea6ce93e9ca1aa2f1cfc89cdc0bbd20e9dec1503
kaleido <kaleido@gmail.com>
2019-05-10 08:49:36 +0900

    signup user!!!!

--- session24-restAPI-server/controllers/auth.js
+++ session24-restAPI-server/controllers/auth.js
@@ -1,4 +1,6 @@
 const { validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+const User = require("../model/user");
 
 exports.signup = (req, res, next) => {
 	const errors = validationResult(req);
@@ -8,7 +10,26 @@ exports.signup = (req, res, next) => {
 		throw error;
 	}
 
-	const emails = req.body.email;
+	const email = req.body.email;
 	const name = req.body.name;
 	const password = req.body.password;
+	bcrypt
+		.hash(password, 12)
+		.then(hashedPassword => {
+			const user = new User({
+				email,
+				name,
+				password: hashedPassword
+			});
+			return user.save();
+		})
+		.then(result => {
+			res.status(201).json({ message: "User created", userId: result._id });
+		})
+		.catch(err => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
 };
