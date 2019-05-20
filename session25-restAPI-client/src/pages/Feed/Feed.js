38eecfac9611e602c910b754185001dce025da90
kaleido <kaleido@gmail.com>
2019-05-20 13:43:21 +0900

    投稿後に同時に更新されるように

--- session25-restAPI-client/src/pages/Feed/Feed.js
+++ session25-restAPI-client/src/pages/Feed/Feed.js
@@ -211,8 +211,8 @@ class Feed extends Component {
 							p => p._id === prevState.editPost._id
 						);
 						updatedPosts[postIndex] = post;
-					} else if (prevState.posts.length < 2) {
-						updatedPosts = prevState.posts.concat(post);
+					} else {
+						updatedPosts.unshift(post);
 					}
 					return {
 						posts: updatedPosts,
